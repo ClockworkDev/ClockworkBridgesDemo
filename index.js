2b73c4d10e5d28089c4a6f7c0508094e2455fe32
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-04-23 18:08:34 +0200

    UWP demo is working

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express()
+
+app.use(express.static(__dirname + '/public'));
+
+app.post('/webbridge', function (req, res) {
+    res.send('Hello World!')
+})
+
+app.listen(process.env.PORT || 80, function () {
+
+})
