fe6e1d2c24fefe64bdb64806b61cb4780e4644c5
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-04-23 18:29:32 +0200

    Azure deployment

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ app.post('/webbridge', function (req, res) {
     res.send('Hello World!')
 })
 
-app.listen(process.env.PORT || 80, function () {
+app.listen(process.env.PORT || 3000, function () {
 
 })
