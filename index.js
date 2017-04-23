9fe9f6bd22255b18feead9889ae1c35568964455
Arcadio Garcia Salvadores <49.pyme@garciamontoro.com>
2017-04-23 19:42:25 +0200

    Web bridge

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 var express = require('express')
 var app = express()
+var multer = require('multer');
+var webBridge = require('clockwork-web-bridge');
+var fs=require('fs');
+
+
+var upload = multer({ dest: 'uploads/' })
 
 app.use(express.static(__dirname + '/public'));
 
-app.post('/webbridge', function (req, res) {
-    res.send('Hello World!')
+app.post('/webbridge', upload.single('package'), function (req, res) {
+    webBridge(req.file.path, "public/webbridgeuploads").then(function (folderName) {
+        fs.unlink(req;.file.path)
+        res.redirect(folderName.replace("public/",""));
+    });
 })
 
 app.listen(process.env.PORT || 3000, function () {
